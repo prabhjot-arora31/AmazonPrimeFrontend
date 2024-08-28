2d7d32fee1882c7eab9d8ade617f64bf4375a3fe
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-08-28 21:16:13 +0530

    Update movieActions.js

--- src/redux/actions/movieActions.js
+++ src/redux/actions/movieActions.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { MOVIE_SUCCESS } from "../constants/movieConstants";
 
 const getMovie = () => async (dispatch) => {
-  await axios.get("/movies").then((response) => {
+  await axios.get("https://amazon-prime-backend.vercel.app/movies").then((response) => {
     console.log("res is:", response.data.result.data);
     dispatch({
       type: MOVIE_SUCCESS,
