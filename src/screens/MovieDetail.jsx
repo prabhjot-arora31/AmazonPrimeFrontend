3d65f26c8d45be70ec2a06c48f288a20b652c018
Prabhjot Arora <83449967+prabhjot-arora31@users.noreply.github.com>
2024-08-29 16:23:52 +0530

    Update MovieDetail.jsx

--- src/screens/MovieDetail.jsx
+++ src/screens/MovieDetail.jsx
@@ -71,7 +71,7 @@ const MovieDetail = () => {
           left: 0,
           width: '100%',
           height: '50%',
-          background: 'linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3), rgba(0,0,0,0.5), rgba(0,0,0,0.7), rgba(0,0,0,0.9)',
+          background: 'linear-gradient(to top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.3), rgba(0,0,0,0.5), rgba(0,0,0,0.7), rgba(0,0,0,0.9))',
           opacity: 0.8,
         }}
       ></div>
